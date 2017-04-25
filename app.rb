47399e481c1a7d4555b8789288d296646d50d358
Anatoliy Klots and Max Reichman <anatoliy@>
2017-04-25 11:46:25 -0700

    Improve how information is retreived and displayed in tamagotchi.erb

--- app.rb
+++ app.rb
@@ -8,12 +8,7 @@ get('/') do
 end
 
 post('/tamagotchi') do
-  @name = params.fetch("name")
-  @tamagotchi = Tamagotchi.new(@name)
-  @pet_name = @tamagotchi.name()
-  @food_level = @tamagotchi.food_level()
-  @sleep_level = @tamagotchi.sleep_level()
-  @activity_level = @tamagotchi.activity_level()
-  @is_alive = @tamagotchi.is_alive()
+  name = params.fetch("name")
+  @tamagotchi = Tamagotchi.new(name)
   erb(:tamagotchi)
 end
